49ddf5a5498d580c71ff0602f7b6f3a355b26205
adrienplg <adrienplg@gmail.com>
2016-02-10 12:28:49 -0800

    First unit test and url mapping, dummy view

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,16 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from lists.views import home_page # View function that return the HTML we want.
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
 
-	def test_bad_maths(self):
-		self.assertEqual(1 + 1, 3)
+	def test_root_url_resolves_to_home_page_view(self):
+		# Django uses resolve() to resolve URLs and find what view function they should map to.
+		found = resolve('/')
+		# Checking tht resolve(), when called with the root of the site, finds a function called home_page
+		self.assertEqual(found.func, home_page)
+
+#class SmokeTest(TestCase):
+#
+#	def test_bad_maths(self):
+#		self.assertEqual(1 + 1, 3)
