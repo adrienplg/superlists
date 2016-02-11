161a99668d43658c987662d0d1572db78f7bf2aa
adrienplg <adrienplg@gmail.com>
2016-02-11 13:17:55 -0800

    Refactor home page view to use a template

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page # View function that return the HTML we want.
 
@@ -15,6 +16,5 @@ class HomePageTest(TestCase):
 	def test_home_page_returns_correct_hmtl(self):
 		request = HttpRequest() # Create an HttpRequest object, which is what Django will see when a browser asks for a page.
 		response = home_page(request) # Pass is to our home_page view, giving back an HttpResponse
-		self.assertTrue(response.content.startswith(b'<html>')) # Using 'b' because response.content is raw bytes, not a Python string
-		self.assertIn(b'<title>To-Do lists</title>', response.content)
-		self.assertTrue(response.content.endswith(b'</html>'))
+		expected_html = render_to_string('home.html')
+		self.assertEqual(response.content.decode(), expected_html)
