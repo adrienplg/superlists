5ee4fb31eba3b57f75ef98ee351c889f34929f54
adrienplg <adrienplg@gmail.com>
2016-02-12 11:42:46 -0800

    Make functional_tests an app, use LiveServerTestCase instead of unittest

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,10 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 import time
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
 	def setUp(self):
 		# Start a selenium webdriver to pop up a real Firefox browser window
@@ -20,7 +21,7 @@ class NewVisitorTest(unittest.TestCase):
 
 	def test_can_start_a_list_and_retrive_it_later(self):
 		# Use the webdriver to open a local webpage
-		self.browser.get('http://localhost:8000')
+		self.browser.get(self.live_server_url)
 
 		# Test assertion that 'To-Do' is in the title and the main header
 		self.assertIn('To-Do', self.browser.title) #
@@ -53,7 +54,4 @@ class NewVisitorTest(unittest.TestCase):
 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')		
 
 		self.fail('Finish the test!') # Fails no matter what. Used as a reminder to finish the test.
-
-if __name__ == '__main__':
-	unittest.main()
 
